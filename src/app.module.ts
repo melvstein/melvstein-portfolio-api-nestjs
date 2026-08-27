6da537e48593e136bd4a76341a7eef657141d88f
melvstein <melvinbayogo@gmail.com>
2026-08-28 00:09:10 +0800

    melvstein portfolio api using nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
-import { AuthModule } from './modules/auth/auth.module.js';
+import { AuthModule } from './core/auth/auth.module.js';
 import { UserModule } from './modules/user/user.module.js';
 
 @Module({
