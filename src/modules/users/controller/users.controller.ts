944519fbb5cd646f10151f4ba2c3cd42d131b69a
melvin.php.ph <melvin.php.ph@tripleonetech.net>
2026-09-03 18:53:03 +0800

    melvstein portfolio api using nestjs

--- src/modules/users/controller/users.controller.ts
+++ src/modules/users/controller/users.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from '../service/users.service.js';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
 
-    constructor(private readonly usersService: UsersService) {}
+  @Get()
+  async getUsers() {
+    return await this.usersService.getUsers();
+  }
 
-    @Get()
-    async getUsers() {
-        return await this.usersService.getUsers();
-    }
+  @Get(':id')
+  async getUserById(@Param('id') id: string) {
+    return await this.usersService.getUserById(id);
+  }
 }
--- src/modules/users/service/users.service.ts
+++ src/modules/users/service/users.service.ts
@@ -3,8 +3,23 @@ import { db } from '../../../database/prisma/db.js';
 
 @Injectable()
 export class UsersService {
-
-    async getUsers() {
-        return await db.orm.public.User.all();
+  async getUsers() {
+    try {
+      return await db.orm.public.User.all();
+    } catch (error) {
+      console.error('Error fetching users:', error);
     }
+
+    return null;
+  }
+
+  async getUserById(id: string) {
+    const user = await db.orm.public.User.where({ id }).first();
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return user;
+  }
 }
