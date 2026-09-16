40d910d1b09c85dfafc513bc4f6911f57040dc74
melvstein <melvinbayogo@gmail.com>
2026-09-16 23:47:52 +0800

    melvstein portfolio api using nestjs

--- src/modules/role/type/role.type.ts
+++ src/modules/role/type/role.type.ts
@@ -3,6 +3,5 @@ import type {
 } from '@prisma/orm-postgres/family-contract/types';
 import type { ResultType } from '@prisma/orm-postgres/components/runtime';
 import { Models } from '../../../database/prisma/contract.d.js';
-import { db } from '../../../database/prisma/db.js';
 
 export type Role = Scalars<Models.public_Role>;
